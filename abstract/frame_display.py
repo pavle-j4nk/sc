658839edf862a096060ee9d2ab169910c6fe6dbc
Pavle Jankovic <pavle.gp@gmail.com>
2019-06-28 20:06:41 +0200

    Abstractions

---
+++ abstract/frame_display.py
@@ -0,0 +1,10 @@
+class FrameDisplay:
+
+    delay = 1
+
+    def set_delay(self, delay: int):
+        self.delay = delay
+
+    def show(self, frame: []):
+        pass
+
---
+++ abstract/frame_filter.py
@@ -0,0 +1,4 @@
+class FrameFilter:
+
+    def do_filter(self, original_frame, filtered_frame, output_frame):
+        return filtered_frame, output_frame
